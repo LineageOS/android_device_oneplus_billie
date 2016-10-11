2aec7cef4291b21675081030fc6f5e411ec4d4f3
Michael Gernoth <michael@gernoth.net>
2016-10-11 13:16:47 +0200

    billie: Exclude AOSP NfcNci
    * We use our own NFC app.
    
    Change-Id: I5345fc03f45844bb10d629f100720e39fbbdf9ee

--- device.mk
+++ device.mk
@@ -452,6 +452,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
