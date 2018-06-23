2abd59fd36575da26044d035251c5fdbd2d8ff8e
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    billie: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    
    Signed-off-by: Demon Singur <demonsingur@gmail.com>

--- device.mk
+++ device.mk
@@ -68,8 +68,10 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.base@1.0
+    android.hidl.manager@1.0_system
 
 # Partitions - Dynamic
 PRODUCT_BUILD_SUPER_PARTITION := false
