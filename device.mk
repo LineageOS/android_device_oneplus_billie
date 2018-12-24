5df97232ded92cdefea94212f711dabd6cc99183
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    billie: Enable QCOM's WFD implementation
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb

--- device.mk
+++ device.mk
@@ -532,3 +532,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
