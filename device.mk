e2a7caab4f64583769ea98c0739d1f61f69a3126
Peter Cai <peter@typeblog.net>
2019-10-11 16:12:06 +0800

    billie: Build lineage power HAL 1.2
    Change-Id: Idd94e6662429afbba612bc2edf8e2f6e9292fa25

--- device.mk
+++ device.mk
@@ -363,6 +363,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service-qti
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
