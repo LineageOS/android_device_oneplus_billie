5c4c850688927a84deb2cc60aba8b97843db8bb7
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-28 22:13:48 +0530

    billie: Build camera hidl hals
    Change-Id: Ic87594e946c41413e8bfe5e7c3c37b67b0fed28a

--- device.mk
+++ device.mk
@@ -169,6 +169,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
