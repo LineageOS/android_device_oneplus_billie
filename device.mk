726bf8e89786d2086339bd0c227816526ffbec87
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    billie: Build new aidl qti power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- device.mk
+++ device.mk
@@ -382,7 +382,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
