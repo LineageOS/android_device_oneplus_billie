4dae031ecf8213f071df3d3856c69edcb696e3b2
LuK1337 <priv.luk@gmail.com>
2020-11-23 15:52:34 +0100

    billie: Build QTI vibrator HAL
    Change-Id: Ic87b5ca1dcf1ce1b4f65e7b90ce8c74a7aeb3ed4

--- device.mk
+++ device.mk
@@ -486,6 +486,9 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service-qti
 
 # Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
