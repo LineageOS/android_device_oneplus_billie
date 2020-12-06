229f7237c47bac3a9bc456d9e0a2e2583e0c2851
shoudil <stephenli@codeaurora.org>
2020-12-06 16:19:10 +0100

    billie: Add framework detect modules to PRODUCT_PACKAGES
    Add JNI and libqti_vndfwk_detect module name to product_packages
    to specify module to be installed to device. Here explicitly
    specify both the core and vendor variant in PRODUCT_PACKAGES.
    
    CRs-Fixed: 2372775
    
    Change-Id: I11e6ed62886c0f8e1c9029a54b6ea166be595fb5

--- device.mk
+++ device.mk
@@ -170,6 +170,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Framework detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
