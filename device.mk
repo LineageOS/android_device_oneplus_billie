92c0d73e854964ce4a588fa2fa2ed6052612f1e8
Erfan Abdi <erfangplus@gmail.com>
2020-12-21 03:34:18 +0330

    billie: Build qti USB hal
    Change-Id: I20db19236d08acbc25a0b68010a138c80863514c

--- device.mk
+++ device.mk
@@ -358,6 +358,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.1-service-qti
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
