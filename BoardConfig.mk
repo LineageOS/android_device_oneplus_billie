8e8899eefba16b7120e128bb2ecfd78f747360ce
danascape <danascape@gmail.com>
2021-09-06 06:56:05 +0200

    billie: Add kernel build flags
    * Set dependencies too
    
    Change-Id: I9b932495c8aa6290598b14516209bf086d5904a9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,29 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_BOOTLOADER_BOARD_NAME := lito
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 2
+
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += service_locator.enable=1
+BOARD_KERNEL_CMDLINE += swiotlb=2048
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 skip_override
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE :=  4096
+BOARD_KERNEL_IMAGE_NAME := Image
+
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+TARGET_KERNEL_CONFIG := vendor/billie-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/oneplus/sm6350
+
 # Platform
 TARGET_BOARD_PLATFORM := lito
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno619L
