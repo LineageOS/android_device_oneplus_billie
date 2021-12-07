f6d1fed23b476d76849b58e47b908fe7884d3c1b
danascape <danascape@gmail.com>
2021-12-07 08:01:31 +0100

    billie: Go enforce
    Change-Id: I5a64ffa86f0083aba5168ab71d033e00f24068e1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,6 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 skip_override
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  4096
