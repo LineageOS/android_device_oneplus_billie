8823d96b27fc4a4038091fb48a5f910b7184f993
danascape <danascape@gmail.com>
2021-12-13 15:08:54 +0530

    billie: Update ramdisk from OOS11
    Change-Id: If9cf1cb46074b2a13473ef4d6eec06ce639d9241

--- rootdir/bin/init.qti.chg_policy.sh
+++ rootdir/bin/init.qti.chg_policy.sh
@@ -1,7 +1,7 @@
 #! /vendor/bin/sh
 
 #
-# Copyright (c) 2019-2020, Qualcomm Technologies, Inc.
+# Copyright (c) 2019-2020 Qualcomm Technologies, Inc.
 # All Rights Reserved.
 # Confidential and Proprietary - Qualcomm Technologies, Inc.
 #
@@ -10,31 +10,39 @@
 
 export PATH=/vendor/bin
 
-if [ -f /sys/devices/soc0/soc_id ]; then
-     soc_id=`cat /sys/devices/soc0/soc_id`
-else
-     soc_id=`cat /sys/devices/system/soc/soc0/id`
+soc_id=`getprop ro.vendor.qti.soc_id`
+if [ "$soc_id" -eq 415 ] || [ "$soc_id" -eq 439 ] || [ "$soc_id" -eq 450 ]; then
+    setprop persist.vendor.hvdcp_opti.start 2
+    exit 0
 fi
 
 if [ "$soc_id" -eq 441 ] || [ "$soc_id" -eq 471 ]; then
 	#Scuba does not support usb-pd or charge pumps
-	find /sys/class/power_supply/battery/ -type f | xargs chown system.system
-	find /sys/class/power_supply/bms/ -type f | xargs chown system.system
-	find /sys/class/power_supply/main/ -type f | xargs chown system.system
-	find /sys/class/power_supply/usb/ -type f | xargs chown system.system
+	find /sys/class/power_supply/battery/ -type f  -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/bms/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/main/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/usb/ -type f -maxdepth 1 | xargs chown system.system
 else
-	find /sys/class/power_supply/battery/ -type f | xargs chown system.system
-	find /sys/class/power_supply/bms/ -type f | xargs chown system.system
-	find /sys/class/power_supply/main/ -type f | xargs chown system.system
-	find /sys/class/power_supply/usb/ -type f | xargs chown system.system
-	find /sys/class/power_supply/charge_pump_master/ -type f | xargs chown system.system
-	find /sys/class/power_supply/pc_port/ -type f | xargs chown system.system
-	find /sys/class/power_supply/dc/ -type f | xargs chown system.system
-	find /sys/class/power_supply/parallel/ -type f | xargs chown system.system
-	find /sys/class/usbpd/usbpd0/ -type f | xargs chown system.system
-	find /sys/class/qc-vdm/ -type f | xargs chown system.system
-	find /sys/class/charge_pump/ -type f | xargs chown system.system
-	find /sys/class/qcom-battery/ -type f | xargs chown system.system
+	find /sys/class/power_supply/battery/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/bms/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/main/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/usb/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/charge_pump_master/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/pc_port/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/dc/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/power_supply/parallel/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/usbpd/usbpd0/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/qc-vdm/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/charge_pump/ -type f -maxdepth 1 | xargs chown system.system
+	find /sys/class/qcom-battery/ -type f -maxdepth 1 | xargs chown system.system
+
+	for i in 0 1 2 3 4 5 6 7 8 9
+	do
+		devname=`cat /sys/bus/iio/devices/iio:device$i/name`
+		if [[ "$devname" == *smb* ]] || [[ "$devname" == *qg* ]] || [[ "$devname" == *div2_cp* ]]; then
+			find /sys/bus/iio/devices/iio:device$i/ -type f -maxdepth 1 | xargs chown system.system
+		fi
+	done
 fi
 
-#setprop persist.vendor.hvdcp_opti.start 1
+setprop persist.vendor.hvdcp_opti.start 1
