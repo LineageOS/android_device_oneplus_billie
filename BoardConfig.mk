afcc6fe00d07616c269690362375d9520996f2b7
danascape <danascape@gmail.com>
2022-01-01 12:45:01 +0530

    billie: Use prebuilt DTC from source
    Change-Id: I2df27971864dc81c2c5a744f37a175096ad76a61

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/billie-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm6350
