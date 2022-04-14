ef714f484b0ac67f87b54bee013b6e2c584a8749
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 21:17:32 -0700

    billie: Import cleaned KONA RROs from IN2025_11.C.33
    Change-Id: I6c668a7fb81fbe69fbdb9c75725bb586010c75d9
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -35,9 +35,11 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
+    FrameworksResTarget \
     SystemUIResCommon \
     TelephonyResCommon \
     WifiResCommon \
+    WifiResTarget \
     OnePlusN10SettingsProvider
 
 # Soong Namespace
