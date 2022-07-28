4221a8ea7f3e6cad8034b4789426cd15a77c2e67
danascape <danascape@gmail.com>
2022-07-28 13:35:32 -0600

    billie: Uprev USB HAL to 1.2
    Change-Id: I9dda6085d2ce481902cd4c0d0e626ce33f1c7946

--- device.mk
+++ device.mk
@@ -497,7 +497,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service-qti
+    android.hardware.usb@1.2-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
