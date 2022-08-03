a80b028ebccdd2fa9ea0862529567b37ecdccb16
danascape <danascape@gmail.com>
2022-08-03 06:54:34 -0600

    billie: Build oneplus display interface from source
    Change-Id: I8d01805d76c2366ac2c6a9490a43ffb609a3bbe8

--- device.mk
+++ device.mk
@@ -249,6 +249,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.16.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
+    vendor.oneplus.hardware.display@1.0.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
