5ffc83921073c0356cc5af2e37a571d004b67552
danascape <danascape@gmail.com>
2022-09-04 07:10:37 -0600

    billie: Switch to QTI vibrator
    * Our device do not use any vibrator effects on stock.
      So there is no need for a custom HAL.
    
    Change-Id: Iafc061c2f21f7be8cd590d7eea65d3a983c1ab2b

--- device.mk
+++ device.mk
@@ -551,7 +551,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.oneplus
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
