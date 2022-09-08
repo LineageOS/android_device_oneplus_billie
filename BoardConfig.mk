6267c59c6b79bcfebb757248ad71d4a86e3807e5
Bruno Martins <bgcngm@gmail.com>
2022-09-08 12:03:17 +0100

    billie: Opt-out of LLVM binutils
    Change-Id: I353dfb264191e587620b4a88f1f6783e54395d6e
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_CONFIG := vendor/billie-perf_defconfig
+TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm6350
 
 # Asserts
