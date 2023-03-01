5dcb8b8640475c5b08e233e5b308eebc78940409
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    billie: Build lineage health HAL and related sepolicies
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -374,6 +374,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.oneplus
