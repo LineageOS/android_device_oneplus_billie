e5df86c192d6ab68eb05c041b45247b2a43c56c6
Georg Veichtlbauer <georg@vware.at>
2023-11-09 14:55:02 +0100

    billie: update-sha1sums: Use Python 3
    Change-Id: I79d6eeea5dc6dc570e72150379befd205a6cb72d
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
