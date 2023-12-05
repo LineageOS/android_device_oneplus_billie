b314fbbd11284bb3ef70c7a972ee0aac11d80433
Saalim Quadri <danascape@gmail.com>
2023-12-05 17:40:04 +0530

    billie: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I3adf55199301768bacb38103c3ef4ba24807df2f
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -285,7 +285,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # fastbootd
 PRODUCT_PACKAGES += \
