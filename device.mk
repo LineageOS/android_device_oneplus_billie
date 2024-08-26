a7f9afbf30aeedbeb589c18b65e8002dad94c38e
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 02:04:54 +0300

    billie: Work around using prebuilt HWC
    After enabling ELF checks and switching prebuilts to modules,
    the init and vintf files are no longer automatically built.
    
    Change-Id: I48fcab53a7c8128b18e1399d9afadcfd5c85b116
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -265,6 +265,8 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service.rc \
+    vendor.qti.hardware.display.composer-service.xml \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
