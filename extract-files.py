f07b0ff365a95453cd9dd1bd7bb7ab5235a6852d
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    billie: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -67,6 +67,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim.so'),
     ('vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so', 'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.bitra.so'): blob_fixup()
         .sig_replace('CF 0A 00 94', '1F 20 03 D5'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
