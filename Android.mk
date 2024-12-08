b7b0b29b4218f05f83bd5b0a75b99db971cbef9a
Saalim Quadri <danascape@gmail.com>
2024-12-09 02:10:56 +0530

    billie: Do not call subdir Makefiles
    * These do not exist anymore
    
    Change-Id: I725294e1cdb396db5018bb50c1f463b2da536008
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- Android.mk
+++ Android.mk
@@ -26,8 +26,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),billie)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 include $(CLEAR_VARS)
 
