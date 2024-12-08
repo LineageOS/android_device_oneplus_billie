3fdfeb81c255d15b7d47b2d9ea5f7d55d80492bd
Saalim Quadri <danascape@gmail.com>
2024-12-09 00:25:24 +0530

    billie: Remove unused init.qcom.efs.sync.sh
    Change-Id: Ic1cc4a395397f2c66b3e6b0377867d44cd3c147b
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -519,7 +519,6 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qti.dcvs.sh \
     init.qcom.early_boot.sh \
-    init.qcom.efs.sync.sh \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
