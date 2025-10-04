1ba78cbb6016c72b7544889f60df1e9da741d87c
Saalim Quadri <danascape@gmail.com>
2025-10-04 15:25:23 +0200

    billie: extract-files: Also copy firmware
    Change-Id: I5885beeb8da68921044eb10c2d70b91ea2d7dc0a
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -78,6 +78,7 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     check_elf=True,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
