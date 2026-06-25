0fd311f1ea1837c65ddfc44419801526045cb748
Saalim Quadri <danascape@gmail.com>
2026-06-25 06:44:03 +0200

    billie: Downgrade to llvm-21
    - Currently kernel does not boot-up with default
      clang-stable (llvm-22)
    
    Change-Id: Iccf84101e220d2cf39b93573e5d9b8ec3aa6060e
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig vendor/billie.config vendor/debugfs.config
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm6350
-
+TARGET_KERNEL_CLANG_VERSION := r563880c
 TARGET_KERNEL_NO_GCC := true
 
 # Asserts
